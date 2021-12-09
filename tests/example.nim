7f98769e404c386fd2fa44f8e2b15699bb4ed92d
xyrise <john.namgung@gmail.com>
2021-12-09 02:13:04 -0500

    Add author field to version
    Author is printed alongside the version, similar to the inclusion of the
    author in `--version` for GNU utilities and the `author` field in Rust's
    `clap` crate.

--- tests/example.nim
+++ tests/example.nim
@@ -2,7 +2,8 @@ import tables
 import clapfn
 
 var parser = ArgumentParser(programName: "mcp", fullName: "My Cool Program",
-                            description: "A test program.", version: "0.0.0")
+                            description: "A test program.", version: "0.0.0",
+                            author: "An Author <author@domain.com>")
 
 # See the wiki for in-depth documentation, especially the purposes
 # of the various parameters.
